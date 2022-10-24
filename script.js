4499e744972813ff6dec3a0f26ed641243728f82
lefthandlou <lulu.bestvater@gmail.com>
2022-10-24 14:44:52 -0400

    Function to display books as they are added to library.

--- script.js
+++ script.js
@@ -7,20 +7,22 @@ let bookAuthor = document.querySelector('.author');
 let bookPages = document.querySelector('.pages');
 let bookRead = document.querySelector('.read');
 
+//Book constructor function
 function Book(title, author, pages, status) {
-    this.title = title
-    this.author = author
-    this.pages = pages
+    this.title = bookTitle.value
+    this.author = bookAuthor.value
+    this.pages = bookPages.value
     this.status = status
 }
 
+//Adds prototype to book constructor to add book to myLibrary array
 Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this);
     console.log(myLibrary);
     return myLibrary;
 }
 
-
+//Event listener to collect input book info
 bookRead.addEventListener('click', () => {
     if (!bookRead.classList.contains('have-read')) {
         bookRead.classList.add('have-read');
@@ -29,6 +31,7 @@ bookRead.addEventListener('click', () => {
     }
 })
 
+//Sets class of book based on read status
 function saveInput() {
     
     let haveRead = "have read";
@@ -37,26 +40,53 @@ function saveInput() {
     if (bookRead.classList.contains('have-read')) {
         let bookReport = haveRead;
         return bookReport;
-    } else {
+    } else {  
         let bookReport = haveNotRead;
         return bookReport;
     }
 }
 
-function bookInfo() {
-    let title = bookTitle.value;
-    let author = bookAuthor.value;
-    let pages = bookPages.value;
+
+//Adds book to library and displays book on shelf
+function shelfBook() {
+
     let read = saveInput();
     let bookInput = new Book(title, author, pages, read);
     console.log(bookInput);
     myLibrary.push(bookInput);
+
+
+
+    let bookShelf = document.querySelector('.bookShelf');
+    let bookDiv = document.createElement('div');
+    let titleDiv = document.createElement('div');
+    let authorDiv = document.createElement('div');
+    let pagesDiv = document.createElement('div');
+    let readDiv = document.createElement('div');
+
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(bookInput));
+    bookShelf.appendChild(bookDiv);
+
+    titleDiv.textContent = bookTitle.value;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+
+    authorDiv.textContent = bookAuthor.value;
+    authorDiv.classList.add('author');
+    bookDiv.appendChild(authorDiv);
+
+    pagesDiv.textContent = bookPages.value;
+    pagesDiv.classList.add('pages');
+    bookDiv.appendChild(pagesDiv);
+
+    readDiv.textContent = saveInput();
+    readDiv.classList.add('read');
+    bookDiv.appendChild(readDiv);
 }
 
-
 submit.addEventListener('click', () => {
-    bookInfo();
-
+    shelfBook();
 })
 
 
